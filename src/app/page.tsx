9b3b1ab02e87fb0df4a6d5e2485fb3b16f06e34b
Muhammad Usman <mu163953@gmail.com>
2025-01-30 12:49:27 +0500

    feat: some style add in hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ import MotivationalPage from "@/components/shared/pageSection/MotivationalPage/M
 export default function Home() {
   return (
     <>
-      {/* <MainQuiz /> */}
-      <MotivationalPage />
+      <MainQuiz />
+      {/* <MotivationalPage /> */}
       {/* <TestimonialSection/> */}
       {/* <MealDeliveryQuiz /> */}
     </>
